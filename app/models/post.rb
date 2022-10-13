f23fa3b57061ba38eae2d3c30ebd2db8d5662978
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-10-13 12:00:54 +0300

    commit tree 7ddaabd3f74dae3af24e227c5106cb63d2e9a51f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :title_should_contain
+    
+
+    TITLE_NOT_CONTAIN = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top/i,
+        /Guess/i
+
+    ]
+
+    def title_should_contain
+        if TITLE_NOT_CONTAIN.none? { |word| word.match title}
+            errors.add(:title, "Title not clickbaity enough")
+        end
+    end
 end
